4aab57312f3b1862b2ae89437b16f2e2d3e6756d
sumit agrawal <sumit4iit@gmail.com>
2016-04-04 00:58:12 +0530

    Modularizing

---
+++ src/Connector.h
@@ -0,0 +1,24 @@
+/*
+ * Connector.h
+ *
+ *  Created on: 03-Apr-2016
+ *      Author: sumit
+ */
+
+#ifndef _CONNECTOR_H_
+#define _CONNECTOR_H_
+#include"include/localTypes.h"
+
+class Connector {
+private:
+	tstring m_tstrHost;
+	uint16_t m_uiPort;
+public:
+	Connector(const tstring& host, const uint16_t port);
+
+	// Returns file descriptor after making connection.
+	virtual int connect();
+	virtual ~Connector();
+};
+
+#endif /* SRC_CONNECTOR_H_ */
---
+++ src/include/globalDefs.h
@@ -0,0 +1,16 @@
+/*
+ * globalDefls.h
+ *
+ *  Created on: 04-Apr-2016
+ *      Author: sumit
+ */
+
+#ifndef SRC_GLOBALDEFLS_H_
+#define SRC_GLOBALDEFLS_H_
+
+
+#define STRINGIFY(x) #x
+#define TOSTR(x) STRINGIFY(x)
+
+
+#endif /* SRC_GLOBALDEFLS_H_ */
