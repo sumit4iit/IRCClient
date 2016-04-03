b4079e605bb8bc283862e65b9eb06a479e3bb507
sumit agrawal <sumit4iit@gmail.com>
2016-04-04 00:55:09 +0530

    Need to get logger in place now! Things are moving quite fast here

--- src/include/localTypes.h
+++ src/include/localTypes.h
@@ -3,12 +3,12 @@
 
 #include<sys/types.h>
 #include<string>
-
-typedef unsigned long uint32_t;
-typedef unsigned short uint16_t;
+#include<stdint.h>
 
 typedef char TCHAR;
 typedef std::string tstring;
 
+//typedef unsigned long uint32_t;
+typedef unsigned short uint16_t;
 
 #endif //__LOCAL_TYPES_H__
